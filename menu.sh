4a4f2fe03de6c1d3ef08ef79a0abc335617f7108
Alouk0 <iamseunaluko@gmail.com>
2026-07-24 18:24:02 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -13,10 +13,11 @@ echo "========================================================"
 echo ""
 echo "  [1] Manage Hysteria 2"
 echo "  [2] Manage Xray (VMess/VLESS/Trojan)"
+echo "  [3] Security & Firewall Settings"
 echo "  [0] Exit"
 echo ""
 echo "========================================================"
-read -p "Select an option [0-2]: " menu_option
+read -p "Select an option [0-3]: " menu_option
 
 case $menu_option in
     1)
@@ -30,6 +31,12 @@ case $menu_option in
         curl -sSL https://raw.githubusercontent.com/Alouk0/autoscript/main/modules/xray.sh -o /root/modules/xray.sh
         chmod +x /root/modules/xray.sh
         bash /root/modules/xray.sh
+        ;;
+    3)
+        mkdir -p /root/modules
+        curl -sSL https://raw.githubusercontent.com/Alouk0/autoscript/main/modules/firewall.sh -o /root/modules/firewall.sh
+        chmod +x /root/modules/firewall.sh
+        bash /root/modules/firewall.sh
         ;;
     0)
         clear; exit 0;;
